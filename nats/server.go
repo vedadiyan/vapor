d353ad88d74fb4a99bce968e853141d56e1826ed
Pouya Vedadiyan <vedadiyan@gmail.com>
2026-08-20 17:20:21 +0800

    code refactor

--- nats/server.go
+++ nats/server.go
@@ -171,3 +171,33 @@ func (r *request) Headers() vapor.KeyValue {
 func newRequest(r *nats.Msg, patten vapor.Pattern, tokens map[string]int) vapor.Request {
 	return &request{Msg: r, tokens: tokens, pattern: patten, ctx: context.Background()}
 }
+
+func toSubject(pattern vapor.Pattern) string {
+	builder := bytes.NewBufferString("")
+
+	for i, seg := range pattern.Segments() {
+		if i > 0 {
+			builder.WriteRune('.')
+		}
+		if strings.HasPrefix(seg, ":") {
+			builder.WriteRune('*')
+			continue
+		}
+		builder.WriteString(seg)
+	}
+
+	return builder.String()
+}
+
+func getParams(subject string, tokens map[string]int) map[string]string {
+	subjectTokens := strings.Split(subject, ".")
+	l := len(subjectTokens)
+	out := make(map[string]string)
+	for key, value := range tokens {
+		if value >= l {
+			continue
+		}
+		out[key] = subjectTokens[value]
+	}
+	return out
+}
